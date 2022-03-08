e40dc6f986173dcc2aa4448d9b5d380fd9a02698
Nived <nivroh2016@gmail.com>
2022-03-08 12:01:30 +0530

    added line graphs

---
+++ plot.py
@@ -0,0 +1,38 @@
+from matplotlib import pyplot as plt
+
+def line_graph(functions):
+    plt.figure(figsize=(20, 11))
+    data = []
+    title = ""
+    for function in functions:
+        value = function()
+        data.append(value)
+    for values in data:
+        plt.plot(values[0], values[1], marker=".", label=values[2])
+        title += values[2] + ", "
+        
+    plt.title(f"India's total COVID-19 {title} Jan 2021 - Aug 2021")
+    plt.xlabel("Months")
+    plt.ylabel("Cases")
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+# gotta fix this
+# def bar_graph(functions):
+#     data = []
+#     title = ""
+#     for function in functions:
+#         value = function()
+#         data.append(value)
+#     for values in data:
+#         plt.bar(values[0], values[1])
+#         title += values[2] + ", "
+        #     plt.title(f"India's total COVID-19 {title} Jan 2021 - Aug 2021")
+#     plt.xlabel("Months")
+#     plt.ylabel("Cases")
+#     plt.grid()
+#     plt.legend()
+#     plt.show()
+
+ 
